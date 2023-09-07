5d4ca99ac67ada9351d58baeaaa299ce8bf6839e
TaisaKiarkhi <tayisiyakiarkhi@gmail.com>
2023-09-07 12:51:37 +0200

    Add project files.

---
+++ Strings/Strings.py
@@ -0,0 +1,21 @@
+print("Giraffe \" Alex")
+print ("Alex \nGiraffe")
+
+phrase= "Hello there"
+
+print(phrase)
+print(phrase  +  " and welcome")
+
+print(phrase.lower())
+print(phrase.upper())
+
+print(phrase.islower())
+print(phrase.lower().islower())
+
+print(len(phrase))
+
+print(phrase[3])
+print(phrase.index("l"))  #this function return the index where the character is placed
+                          #if there are several similar character, the function
+                          #will return the first one that encounters
+
